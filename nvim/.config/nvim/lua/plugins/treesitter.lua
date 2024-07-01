d9e57f6245a7c78a9e015abb4dd0ecce2f724954
dan5 <daniel.sarracayo@gmail.com>
2024-07-01 21:35:24 +0100

    nvim - telescope commands and treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,24 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate"
+	build = ":TSUpdate",
+	config = function ()
+		require('nvim-treesitter.configs').setup {
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"bash",
+				"javascript",
+				"typescript",
+				"css",
+				"html" 
+			},
+			highlight = {
+				enable = true
+			},
+			indent = {
+				enable = true
+			}
+		}
+	end
 }
