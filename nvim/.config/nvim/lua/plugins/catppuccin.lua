add4e13daf7074ad0014203a53d7344820d25ac3
Dan <daniel.sarracayo@gmail.com>
2024-07-02 09:33:40 +0100

    alacritty - added config and catppuccin mocha

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,10 +2,11 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-	config = function ()
-		require("catppuccin").setup()
-
-		-- setup must be called before loading
-		vim.cmd.colorscheme "catppuccin"
-	end
+  config = function ()
+    require("catppuccin").setup {
+			flavor = "mocha"	
+    }
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
+  end
 }
