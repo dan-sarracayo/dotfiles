b1c0543ce215d70828631c4bb314e397e0efe2cc
dan5 <daniel.sarracayo@gmail.com>
2024-07-05 20:50:26 +0100

    nvim: added neotree and keymaps.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Allow esc from terminal.
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap=true })
 
+-- Neotree keymaps.
+vim.keymap.set('n', '<leader>ft', '<Cmd>Neotree toggle<CR>')
+
 -- Telescope keymaps.
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
