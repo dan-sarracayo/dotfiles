3af6295c4ce4af5d6ae42c1c6a0dea27191974b5
Dan Sarracayo <daniel.sarracayo@gmail.com>
2024-07-05 17:16:54 +0100

    nvim: added no mouse option

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,11 @@
 -- Some creature comforts.
-vim.cmd("set nu")
-vim.cmd("syntax on")
-vim.cmd("set ttyfast")
-vim.cmd("set autoindent")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=0")
+vim.cmd("set nu") -- line numbers.
+vim.cmd("syntax on") -- syntax on.
+vim.cmd("set ttyfast") -- quicker nav.
+vim.cmd("set autoindent") -- indent on new lines.
+vim.cmd("set tabstop=2") -- tabs to two space width.
+vim.cmd("set shiftwidth=0") -- use tab stop.
+vim.cmd("set mouse=") -- no mouse
 
 -- True colours.
 vim.o.termguicolors = true
