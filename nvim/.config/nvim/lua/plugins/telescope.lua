305244f24fec573279230506ca391cc07a603164
dan5 <daniel.sarracayo@gmail.com>
2024-07-17 09:43:21 +0100

    nvim: telescope show hidden

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,12 +2,18 @@ return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function ()
-		require('telescope').setup{ 
+		require('telescope').setup { 
   		defaults = { 
     		file_ignore_patterns = { 
-      		"node_modules" 
+      		"node_modules",
+					".git"
     		}
-  		}
+  		},
+			pickers = {
+				find_files = {
+					hidden = true
+				}
+			}
 		}
 	end
 }
