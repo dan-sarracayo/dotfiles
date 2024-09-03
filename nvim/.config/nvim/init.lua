8d1d570286a63094a56c21df466660968ddae9d6
Dan Sarracayo <daniel.sarracayo@gmail.com>
2024-09-03 15:47:44 +0100

    nvim: added list options for whitespace chars

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.cmd("set autoindent") -- indent on new lines.
 vim.cmd("set tabstop=2") -- tabs to two space width.
 vim.cmd("set shiftwidth=0") -- use tab stop.
 vim.cmd("set mouse=") -- no mouse
+vim.cmd("set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<")
 
 -- True colours.
 vim.o.termguicolors = true
